07bf3c8293851648c6b0a3993f762263b19519ac
sam <ghsamm@gmail.com>
2019-08-18 18:09:07 +0100

    paginate products using a button

--- public/client/App.js
+++ public/client/App.js
@@ -5,27 +5,48 @@ const App = () => {
   const [products, setProducts] = useState([]);
   const [pageNumber, setPageNumber] = useState(1);
   const sortByOption = sortByOptions[activeSortByOptionId];
+
+  const changeSortByOptionAndResetPageNumber = sortByOptionId => {
+    setActiveSortByOptionId(sortByOptionId);
+    setPageNumber(1);
+  };
+  const concatProducts = newProducts => {
+    setProducts(products.concat(newProducts));
+  };
   const fetchProducts = () => {
     fetch(
       `http://localhost:3000/api/products?_page=${pageNumber}${sortByOption &&
         "&_sort=" + sortByOption}`
     )
       .then(res => res.json())
-      .then(data => setProducts(data));
+      .then(newProducts => {
+        if (pageNumber === 1) {
+          setProducts(newProducts);
+        } else {
+          concatProducts(newProducts);
+        }
+      });
   };
 
   useEffect(() => {
     fetchProducts();
-  }, [pageNumber, sortByOption]);
+  }, [pageNumber, activeSortByOptionId]);
 
   return (
     <div className="app-container">
       <Header
         sortByOptions={sortByOptions}
         activeSortByOptionId={activeSortByOptionId}
-        onClickOption={setActiveSortByOptionId}
+        onClickOption={changeSortByOptionAndResetPageNumber}
       />
       <ProductList products={products} />
+      <input
+        type="button"
+        value="more"
+        onClick={() => {
+          setPageNumber(pageNumber + 1);
+        }}
+      />
     </div>
   );
 };
