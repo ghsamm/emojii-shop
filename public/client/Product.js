44e406cf23e681ef2383d491eb246bdaf1447d85
sam <ghsamm@gmail.com>
2019-08-18 13:53:29 +0100

    finish styling of products
    I am a big fan of styled-components, they make styling React components
    much easier
    however, in this example I used vanilla CSS with a naming convention
    called BEM http://getbem.com/introduction/
    
    BEM reduces the risk of naming conflicts and, most importantly, CSS
    specificity bugs
    
    I also made sure the app is responsive from the beginning
    
    I added a cute preview animation for the faces, I will probably add more
    animations and randomise them later on

--- public/client/Product.js
+++ public/client/Product.js
@@ -1,3 +1,21 @@
+const renderSize = size => {
+  return `${size}px`;
+};
+
+const renderPrice = price => {
+  return `$${price.toFixed(2)}`;
+};
+
 const Product = ({ product: { id, size, price, face, date } }) => {
-  return face;
+  return (
+    <div className="product__container">
+      <div className="product__face" style={{ fontSize: size }}>
+        {face}
+      </div>
+      <div className="product__details">
+        <div className="product__size">{renderSize(size)}</div>
+        <div className="product__price">{renderPrice(price)}</div>
+      </div>
+    </div>
+  );
 };
